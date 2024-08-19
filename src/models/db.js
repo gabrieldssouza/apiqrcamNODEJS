1bfa142bd61cf20f791d9ee3b164272b8dc4f74d
Gabriel Souza <154625283+gabrieldssouza@users.noreply.github.com>
2024-08-18 22:31:33 -0300

    Add files via upload

---
+++ src/models/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'mysql.infocimol.com.br',
+    user: 'infocimol07',
+    password: 'qrcam123',
+    database: 'infocimol07'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar no banco de dados: ' + err);
+        return;
+    }
+});
+
+module.exports = db;
