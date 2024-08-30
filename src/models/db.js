b546419ed7205d7e55c936ed686bbfaca762c64e
gabrieldssouza <gabrieldssouza@icloud.com>
2024-08-30 11:09:20 -0300

    Primeiro commit

---
+++ src/models/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'mysql.infocimol.com.br',
+    user: 'infocimol07',
+    password: 'qrcam123',
+    database: 'infocimol07',
+    connectTimeout: 10000 
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar no banco de dados: ' + err);
+        return;
+    }
+});
+
+module.exports = db;
